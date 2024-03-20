2a0680addf737389223f6fc8fd5d76be0cc20743
manel naloufi <63862628+naloufi-manel@users.noreply.github.com>
2024-03-20 13:37:17 +0100

    Update main.py

--- Lora-network/main.py
+++ Lora-network/main.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-# lien de la video pour le serveur : https://www.google.com/search?q=how+to+execute+a+code+in+the+cloud+for+ever&sxsrf=ALiCzsYN5ZDVmmQFs6FX0mFPbaAXhI7bkQ%3A1659719820702&ei=jFDtYqi0KoWEaYHtqrgH&ved=0ahUKEwioqtuSmrD5AhUFQhoKHYG2CncQ4dUDCA0&uact=5&oq=how+to+execute+a+code+in+the+cloud+for+ever&gs_lcp=Cgdnd3Mtd2l6EAMyBwghEKABEAo6BwgAEEcQsAM6BQghEKABOggIIRAeEBYQHToECCEQFUoECEEYAEoECEYYAFDiB1i4FmDXF2gBcAF4AIABtgGIAawHkgEDNS40mAEAoAEByAEIwAEB&sclient=gws-wiz#kpvalbx=_o1DtYuHDEtGMlwTVj4SwCA18
-# cd Bureau/capteur/lora
-## execution : python3  storage_data.py
 
 from keep_alive import keep_alive 
 import json
@@ -13,9 +10,9 @@ import shutil
 import smtplib
 import ssl
 import socket
-#### install  pyopenssl #  pip install -U pyopenssl # Successfully installed cryptography-37.0.4 pyopenssl-22.0.0
+#### install  pyopenssl :  pip install -U pyopenssl # Successfully installed cryptography-37.0.4 pyopenssl-22.0.0
 
-# configuration de la connection a storage de TTN
+# TTN storage connection configuration
 id_device = "eui-70b3d57ed0051a0b"
 APIKey = "NNSXS.PZFW4LAIIPRAXCDZKME6QVGNH6WSLOUNXAP3XQQ.MITEOENWAUD7WTKQTS66I2VA5VOP2E4TQE4VALELYHJOQJILIKTQ"
 Fields = "up.uplink_message.decoded_payload,up.uplink_message.frm_payload"
@@ -26,10 +23,10 @@ Header = { "Accept": "text/event-stream", "Authorization": "Bearer " + APIKey ,'
 print("\n\nFetching from data storage...\n")
 
 
-## Configuration de l'envoie du mail
+## Mail sending configuration
 password = "hcekxxykhoycrkym"    # Your app password goes here
-sender = "naloufi.send@gmail.com"    # Your e-mail address
-receiver = "naloufi.send@gmail.com" # Recipient's address
+sender = "email@gmail.com"    # Your e-mail address
+receiver = "email@gmail.com" # Recipient's address
 
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.ehlo()
@@ -38,11 +35,12 @@ server.ehlo()
 
 server.login(sender,password)
 
+# Warning message
 body = """
-Attention le capteur ne transmet plus depuis plus de 20 min.
+Warning: the sensor has not been transmitting for more than 20 min.
 """
 
-# fonction hashfile 
+# Fonction hashfile 
 def hashfile(filename):
 	m = hashlib.md5()
 	with open(filename, 'rb') as f:
@@ -50,7 +48,7 @@ def hashfile(filename):
 	return ''.join('%02x' % ord(b) for b in m.digest())
 
 
-### faire le premier fichier (si le dossier est vide) # recuperer les données 
+### Make first file (if folder is empty) # Retrieve data 
 if len(os.listdir('file')) == 0:
 	print("Le répertoire est vide")
 	r = requests.get(URL, headers = Header)
@@ -79,7 +77,7 @@ while True:
 		time.sleep(5)
 		print("continue...")
 		continue"""
-	# ne marche pas 	
+	# doesn't work
 	#except ValueError:
         #	print("Oops!  That was no valid number.  Try again...")
 
@@ -110,7 +108,7 @@ while True:
   F1.close()
   file1.close()
   result = filecmp.cmp("file/file4.txt","file/file3.txt", False) 
-  print(result) # true si identique 
+  print(result) # true if identical
 
 
   if result == False : 
@@ -148,7 +146,7 @@ while True:
 	
   print("Ok")		
   print(i)
-  if i==2880 : # 2880 ## sauvgarde dans archive toute les 24h (avec un nom de fichier en boucle)
+  if i==2880 : # 2880 ## save in archive every 24h (with a looping file name)
     original=r'file/file.txt'
     target=r'archive/file_'+ str(l) +'.txt'	
     shutil.copyfile(original,target)
@@ -157,7 +155,7 @@ while True:
 		#print("sauvgarde")
 	
   if (no_data>40 and flag_notif==False): 
-		# send a message (si plus de 20 minute sans recuperation de données 
+		# send a message (if more than 20 minutes without data recovery)
     flag_notif=True
     try :
       server.sendmail(sender, receiver, body)
